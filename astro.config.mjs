8a43263b4fe642dad4509f643cd5b7fdb0f1af68
Biraj <biraj.pub@gmail.com>
2026-09-04 23:13:52 -0400

    refactor: biraj.wtf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://biraj.wtf',
+  integrations: [sitemap()],
   compressHTML: false,
   redirects: {
     '/blogs': '/posts',
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/#sitemap-link-in-robotstxt
+
+import type { APIRoute } from "astro";
+
+const getRobotsTxt = (sitemapURL: URL) => `\
+User-agent: *
+Allow: /
+
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL("sitemap-index.xml", site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
